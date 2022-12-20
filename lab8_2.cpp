f92d89288018af5879f3f98e418ad6ef08c6a6e1
= <=>
2022-12-20 15:25:09 +0700

    readme stdid

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,4 +1,26 @@
 //Dialog of the first example is given below.
+#include<iostream>
+using namespace std ;
+
+
+int main() {
+    string name ;
+    int studentpass,gear,current ;
+cout<<"Fahsai: Sawadee ka...Can you tell me your name?\n" ;
+getline(cin,name) ;
+cout<<"?????: "<<name ;
+cout<<"Fahsai: Wow!!! "<<name<<" is a really cool name.\n" ;
+cout<<"Fahsai: I think you are an Engineering student. What is your student ID?\n"
+cout<<name<<": " ;
+cin>>studentpass ;
+
+}
+
+
+
+
+
+//Dialog of the first example is given below.
 
 /*
 Fahsai: Sawadee ka...Can you tell me your name?
